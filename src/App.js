e0e70fec7be904dc846c07cc435dacfa340dd2b1
Mikhail Mordik <ahsimm1@gmail.com>
2023-08-09 12:33:37 +0000

    update fetch request function

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import Sort from './components/Sort';
 import Categories from './components/Categories';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzas from './assets/pizza.json';
+//import pizzas from './assets/pizza.json';
 
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://64d36c0a67b2662bf3dc31bb.mockapi.io/api/v1/items')
+    .then(res =>  res.json())
+    .then(arr => {
+      setItems(arr);
+  })
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -21,7 +31,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock
                 key={obj.id}
                 title={obj.title}
