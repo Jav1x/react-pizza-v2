dad8fe538a5d4d6fbd84da285c35930dbfc25086
Jav1x <ahsimm1@gmail.com>
2023-08-11 17:06:03 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header';
-import Sort from './components/Sort';
-import Categories from './components/Categories';
-import PizzaBlock from './components/PizzaBlock';
-import Skeleton from './components/PizzaBlock/Skeleton';
+import Header from "./components/Header";
+import Sort from "./components/Sort";
+import Categories from "./components/Categories";
+import PizzaBlock from "./components/PizzaBlock";
+import Skeleton from "./components/PizzaBlock/Skeleton";
 
-
-import './scss/app.scss';
+import "./scss/app.scss";
 
 function App() {
   const [isLoading, setIsLoading] = React.useState(true);
   const [items, setItems] = React.useState([]);
 
   React.useEffect(() => {
-    fetch('https://64d36c0a67b2662bf3dc31bb.mockapi.io/api/v1/items')
-    .then(res =>  res.json())
-    .then(arr => {
-      setItems(arr);
-  })
+    fetch("https://64d36c0a67b2662bf3dc31bb.mockapi.io/api/v1/items")
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
@@ -32,16 +32,18 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {isLoading 
-            ? [ ... new Array(6)].map((_, index) => <Skeleton key={index} />)
-             : items.map((obj) => <PizzaBlock
-             key={obj.id}
-             title={obj.title}
-             price={obj.price}
-             imgUrl={obj.imageUrl}
-             sizes={obj.sizes}
-             types={obj.types}
-           />)} 
+            {isLoading
+              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+              : items.map((obj) => (
+                  <PizzaBlock
+                    key={obj.id}
+                    title={obj.title}
+                    price={obj.price}
+                    imgUrl={obj.imageUrl}
+                    sizes={obj.sizes}
+                    types={obj.types}
+                  />
+                ))}
           </div>
         </div>
       </div>
